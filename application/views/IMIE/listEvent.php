8edefc6a3c94f3ef1b4943c501c21340681fee0e
DANIEL-Dereck <daniel.dereck@gmail.com>
2016-09-20 13:56:34 +0200

    ajout model et view pour lsit event

---
+++ application/views/IMIE/listEvent.php
@@ -0,0 +1,61 @@
+        <main class="container">
+            <div class="container space-correc">
+                <a href="index.php?action=aboutUs" class="boxhp pull-left col-md-4"><div><p>DECOUVREZ NOUS</p></div></a>
+                <div class="pull-right col-md-8">
+                    <div class="homepagetxtblock ">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim magna, hendrerit quis ipsum et, consequat posuere quam. Nunc aliquam ex id velit ultricies rutrum. Vivamus rutrum risus a molestie euismod. Nullam tincidunt, tellus eu consectetur convallis, nibh orci fringilla lacus, et egestas ante dui ac sem. Maecenas facilisis, leo nec feugiat finibus, orci velit semper libero, et accumsan risus magna sit amet dolor. Donec dapibus elit nulla, eu ultrices diam eleifend non. Vivamus id volutpat magna. Fusce feugiat ultricies orci aliquet congue. Pellentesque eget dapibus mi. Aenean aliquet in mi et egestas. Aliquam ut risus tempus, tempus quam nec, dapibus tortor.
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="container space-correc">
+                <div class="homepagetxtbloack pull-left col-md-8">
+                    <div id="myCarousel" class="carou-resize carousel slide" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner container" role="listbox">
+
+                            <div class="item active">
+                                <img class="img-responsive img-resize" src="assets/img/ani1.jpg" alt="img1">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-resize" src="assets/img/ani2.jpg" alt="img2">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-resize" src="assets/img/ani3.jpg" alt="img3">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item align-center">
+                                <img class="img-responsive img-resize" src="assets/img/download.jpg" alt="img4">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item align-center">
+                                <img class="img-responsive img-resize" src="assets/img/1_malevitch.jpg" alt="img5">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                            <div class="item align-center">
+                                <img class="img-responsive img-resize" src="assets/img/moi.jpg" alt="img6">
+                                <div class="carousel-caption">
+                                </div>
+                            </div>
+                        </div>
+
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <a href="index.php?action=listEvent" class="boxhp pull-right col-md-4"><div><p>PROCHAIN EVENEMENT</p></div></a>
+            </div>
+        </main>
