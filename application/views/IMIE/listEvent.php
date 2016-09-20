5617195981b317d59d0531eb3f52ed1891ec1851
DANIEL-Dereck <daniel.dereck@gmail.com>
2016-09-20 14:47:13 +0200

    petit ajout

--- application/views/IMIE/listEvent.php
+++ application/views/IMIE/listEvent.php
@@ -1,61 +1,37 @@
-        <main class="container">
-            <div class="container space-correc">
-                <a href="index.php?action=aboutUs" class="boxhp pull-left col-md-4"><div><p>DECOUVREZ NOUS</p></div></a>
-                <div class="pull-right col-md-8">
-                    <div class="homepagetxtblock ">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim magna, hendrerit quis ipsum et, consequat posuere quam. Nunc aliquam ex id velit ultricies rutrum. Vivamus rutrum risus a molestie euismod. Nullam tincidunt, tellus eu consectetur convallis, nibh orci fringilla lacus, et egestas ante dui ac sem. Maecenas facilisis, leo nec feugiat finibus, orci velit semper libero, et accumsan risus magna sit amet dolor. Donec dapibus elit nulla, eu ultrices diam eleifend non. Vivamus id volutpat magna. Fusce feugiat ultricies orci aliquet congue. Pellentesque eget dapibus mi. Aenean aliquet in mi et egestas. Aliquam ut risus tempus, tempus quam nec, dapibus tortor.
+<main>
+           <?php
+            $rsql_event = 'SELECT * FROM event';
+            foreach ($bdd->query($rsql_event) as $row) {
+                ?>
+
+                <div onclick="changeVisu(this, 'container event-list-click', 'container event-list');" class="container event-list">
+                    <div class="col-md-11">
+                        <h4><b><?php echo($row[1]); ?></b></h4>
+                        <span>
+                            <b>Date de l'evenement : </b><?php echo(whodate($row[3])); ?><br/>
+                            <?php
+                            if ($row[4] != null) {
+                                print("<b>Date de fin de l'evenement : </b>" . whodate($row[4]));
+                            } else {
+                                
+                            }
+                            ?>
+                            <br/>
+                            <b>Lieux: </b><?php echo 'palce de ttintin'; ?>
+                        </span>
+                        <p>
+                            <b>Description : </b><?php echo($row[2]); ?><br/>
+                            <b><?php echo 'Tarif : 68€'; ?></b>
+                        </p>
+                    </div>
+                    <div class="col-md-1">
+                        <?php #afficher les 2 en desous si admin seulement   ?>
+                        <a class="gear col-md-4" href="index.php"><i class="fa fa-cog" aria-hidden="true"></i></a>
+                        <?php #afficher '+' si user inscrit a l'event sinon afficher '-'   ?>
+                        <a class="minus col-md-4" href="index.php"><i class="fa fa-minus" aria-hidden="true"></i></a>
+                        <a class="plus col-md-4" href="index.php"><i class="fa fa-plus" aria-hidden="true"></i></a>
                     </div>
                 </div>
-            </div>
-            <div class="clearfix"></div>
-            <div class="container space-correc">
-                <div class="homepagetxtbloack pull-left col-md-8">
-                    <div id="myCarousel" class="carou-resize carousel slide" data-ride="carousel">
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner container" role="listbox">
 
-                            <div class="item active">
-                                <img class="img-responsive img-resize" src="assets/img/ani1.jpg" alt="img1">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item">
-                                <img class="img-responsive img-resize" src="assets/img/ani2.jpg" alt="img2">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item">
-                                <img class="img-responsive img-resize" src="assets/img/ani3.jpg" alt="img3">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item align-center">
-                                <img class="img-responsive img-resize" src="assets/img/download.jpg" alt="img4">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item align-center">
-                                <img class="img-responsive img-resize" src="assets/img/1_malevitch.jpg" alt="img5">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                            <div class="item align-center">
-                                <img class="img-responsive img-resize" src="assets/img/moi.jpg" alt="img6">
-                                <div class="carousel-caption">
-                                </div>
-                            </div>
-                        </div>
-
-                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-                <a href="index.php?action=listEvent" class="boxhp pull-right col-md-4"><div><p>PROCHAIN EVENEMENT</p></div></a>
-            </div>
-        </main>
+            <?php } ?>
+</main>
